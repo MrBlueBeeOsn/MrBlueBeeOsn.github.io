3c87ea05921a8b63cb97c676631f50bed7caead7
🐝 Mr. ßee - 🅾ng siêng năng📘 <130443635+MrBlueBeeOsn@users.noreply.github.com>
2025-09-20 17:52:00 +0000

    ye

--- my-react-app/src/assets/jsx/English.jsx
+++ my-react-app/src/assets/jsx/English.jsx
@@ -4,11 +4,13 @@ export default function English() {
   return (
     <div>
      
-      <main className="image">
+      <main class="image image1 content">
 
         <h1 className="margin-y-50 text-center">English</h1>
 
       </main>
+
+      <button className="back-to-top" title="Back to top"></button>
 
     </div>
   );
