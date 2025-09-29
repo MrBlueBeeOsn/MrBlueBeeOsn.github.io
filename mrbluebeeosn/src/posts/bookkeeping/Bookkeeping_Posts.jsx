31134567e5777bc5b25511666e15d828e7a3df85
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-09-29 23:00:39 +0700

    ye

--- mrbluebeeosn/src/posts/bookkeeping/Bookkeeping_Posts.jsx
+++ mrbluebeeosn/src/posts/bookkeeping/Bookkeeping_Posts.jsx
@@ -24,6 +24,46 @@ export default function Bookkeeping_Posts() {
       content: "Accounts receivable (AR), abbreviated as A/R sometimes, represent the money owed to a business by its customers for goods or services that have been delivered but not yet paid for.",
       slug: "bookkeeping/what-is-accounts-receivable?",
       date: "2024-02-22"
+    },
+    { 
+      id: 1.1004,
+      category: "Bookkeeping",
+      title: "Accounts Payable", 
+      content: "Accounts payable (AP), sometimes abbreviated as A/P, refers to the short-term debt a business owes to its suppliers for goods or services that have been received but not yet paid for.",
+      slug: "bookkeeping/what-is-accounts-payable",
+      date: "2024-02-22"
+    },
+    { 
+      id: 1.1005,
+      category: "Bookkeeping",
+      title: "Journal entry", 
+      content: "A journal entry is the fundamental record of a financial transaction in accounting.",
+      slug: "bookkeeping/what-is-a-journal-entry",
+      date: "2024-02-27"
+    },
+    { 
+      id: 1.1006,
+      category: "Bookkeeping",
+      title: "General ledger", 
+      content: "A general ledger is a comprehensive record of all the financial transactions of a company.",
+      slug: "bookkeeping/what-is-a-general-ledger",
+      date: "2024-03-18"
+    },
+    { 
+      id: 1.1007,
+      category: "Bookkeeping",
+      title: "Trial balance", 
+      content: "A trial balance in accounting is like a giant checklist to ensure everything is balanced in the company's books.",
+      slug: "bookkeeping/what-is-a-trial-balance",
+      date: "2024-02-28"
+    },
+    { 
+      id: 1.1008,
+      category: "Bookkeeping",
+      title: "Account reconciliation", 
+      content: "Account reconciliation in accounting is like double-checking your piggy bank to make sure everything adds up.",
+      slug: "bookkeeping/what-is-account-reconciliation",
+      date: "2024-04-22"
     }
   ];
 }
--- mrbluebeeosn/src/posts/english_/Grammar_Posts.jsx
+++ mrbluebeeosn/src/posts/english_/Grammar_Posts.jsx
@@ -12,18 +12,26 @@ export default function Grammar_Posts() {
     { 
       id: 2.2002,
       category: "Grammar",
-      title: "",
-      content: "",
-      slug: "",
-      date: "2024-04-25"
+      title: "Verb",
+      content: "A verb in English is an action word! It tells you what someone or something is doing, or what happens.",
+      slug: "english/grammar/what-is-a-verb",
+      date: "2024-07-09"
     },
     { 
       id: 2.2003,
       category: "Grammar",
-      title: "",
-      content: "",
-      slug: "",
-      date: "2024-09-07"
+      title: "Adjective",
+      content: "An adjective in English is a describing word! It helps you tell more about the nouns (people, places, things) in your sentence.",
+      slug: "english/grammar/what-is-an-adjective",
+      date: "2024-07-09"
+    },
+    { 
+      id: 2.2003,
+      category: "Grammar",
+      title: "Noun",
+      content: "A noun in English is a word that names a person, place, thing, or idea.",
+      slug: "english/grammar/what-is-a-noun",
+      date: "2024-07-09"
     }
   ];
 }
