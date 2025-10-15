502d9bcc5baefff0ab93d1f62baa326a49e5d367
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-10-15 20:53:11 +0700

    ye

---
+++ mrbluebeeosn/src/legacy/components/EnglishTopic.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
+
+export default function EnglishTopic() {
+  return (
+
+    <div className="topic-container">
+
+      {/* =============================
+        1. English Learning Terms
+      ============================= */}
+
+      <div  className="flex-topic">
+
+        <HashLink smooth to="/english">
+        <mark className="highlight-tertiary-padding-2-4">English</mark>
+        </HashLink>
+
+        <HashLink smooth to="/grammar">
+          <mark className="highlight-tertiary-padding-2-4">Grammar</mark>
+        </HashLink>
+
+        <HashLink smooth to="/pronunciation">
+          <mark className="highlight-tertiary-padding-2-4">Pronunciation</mark>
+        </HashLink>
+
+        <HashLink smooth to="/tieng-anh">
+          <mark className="highlight-tertiary-padding-2-4">Tiếng Anh</mark>
+        </HashLink>
+
+        <HashLink smooth to="/vocabulary">
+          <mark className="highlight-tertiary-padding-2-4">Từ vựng</mark>
+        </HashLink>
+
+      </div>
+
+    </div>
+
+  );
+}
