a27b4371a72db953a3dc24e0421ca7f4a21240d9
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-10-17 21:42:26 +0700

    ye

--- mrbluebeeosn/src/AppRoutes.tsx
+++ mrbluebeeosn/src/AppRoutes.tsx
@@ -2,94 +2,95 @@
 // const  = lazy(() => import(''))
 // <Suspense fallback={<></>}></Suspense>
 
-import { lazy, Suspense } from 'react'
+import React from 'react';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-const Layout = lazy(() => import('./components/Layout'))
-const Posts = lazy(() => import('./data/Posts'))
+const Layout = lazy(() => import('@/components/Layout'));
+const Posts = lazy(() => import('@/data/Posts'));
 
 // 1_header import các chức năng và đường dẫn
-  const Home = lazy(() => import('./1_header/Home'))
-  const SearchIndex = lazy(() => import('./1_header/SearchIndex'))
+  const Home = lazy(() => import('@/1_header/Home'));
+  const SearchIndex = lazy(() => import('@/1_header/SearchIndex'));
 
 // 2_main import các chức năng và đường dẫn
-  const MrBeeOsn = lazy(() => import('./2_main/msbeeosn/MrBeeOsn'))
-  const AboutIndex = lazy(() => import('./2_main/msbeeosn/_indexes/AboutIndex'))
-  const ProjectIndex = lazy(() => import('./2_main/msbeeosn/_indexes/ProjectIndex'))
-  const JourneyIndex = lazy(() => import('./2_main/msbeeosn/_indexes/JourneyIndex'))
+  const MrBeeOsn = lazy(() => import('@/2_main/msbeeosn/MrBeeOsn'));
+  const AboutIndex = lazy(() => import('@/2_main/msbeeosn/_indexes/AboutIndex'));
+  const ProjectIndex = lazy(() => import('@/2_main/msbeeosn/_indexes/ProjectIndex'));
+  const JourneyIndex = lazy(() => import('@/2_main/msbeeosn/_indexes/JourneyIndex'));
 
 // 2_main import các chức năng và đường dẫn Bookkeeping
 
   // 1. Bookkeeping
-  const BookkeepingIndex = lazy(() => import('./2_main/bookkeeping/_indexes/BookkeepingIndex'))
-  const Bookkeeping = lazy(() => import('./2_main/bookkeeping/Bookkeeping'))
-  const Bookkeeper = lazy(() => import('./2_main/bookkeeping/Bookkeeper'))
-  const AccountsReceivable = lazy(() => import('./2_main/bookkeeping/AccountsReceivable'))
-  const AccountsPayable = lazy(() => import('./2_main/bookkeeping/AccountsPayable'))
-  const JournalEntry = lazy(() => import('./2_main/bookkeeping/JournalEntry'))
-  const GeneralLedger = lazy(() => import('./2_main/bookkeeping/GeneralLedger'))
-  const TrialBalance = lazy(() => import('./2_main/bookkeeping/TrialBalance'))
-  const AccountReconciliation = lazy(() => import('./2_main/bookkeeping/AccountReconciliation'))
+  const BookkeepingIndex = lazy(() => import('@/2_main/bookkeeping/_indexes/BookkeepingIndex'));
+  const Bookkeeping = lazy(() => import('@/2_main/bookkeeping/Bookkeeping'));
+  const Bookkeeper = lazy(() => import('@/2_main/bookkeeping/Bookkeeper'));
+  const AccountsReceivable = lazy(() => import('@/2_main/bookkeeping/AccountsReceivable'));
+  const AccountsPayable = lazy(() => import('@/2_main/bookkeeping/AccountsPayable'));
+  const JournalEntry = lazy(() => import('@/2_main/bookkeeping/JournalEntry'));
+  const GeneralLedger = lazy(() => import('@/2_main/bookkeeping/GeneralLedger'));
+  const TrialBalance = lazy(() => import('@/2_main/bookkeeping/TrialBalance'));
+  const AccountReconciliation = lazy(() => import('@/2_main/bookkeeping/AccountReconciliation'));
 
 // 2_main import các chức năng và đường dẫn
 
   // Mr.Bee Osn English Terms
-  const SearchingAndClickableDictionary = lazy(() => import('./2_main/english/mrbeeenglish/SearchingAndClickableDictionary'))
+  const SearchingAndClickableDictionary = lazy(() => import('@/2_main/english/mrbeeenglish/SearchingAndClickableDictionary'));
 
   // 1. English
-  const EnglishIndex = lazy(() => import('./2_main/english/_indexes/EnglishIndex'))
-  const English = lazy(() => import('./2_main/english/English'))
-  const TheEnglishLanguage = lazy(() => import('./2_main/english/TheEnglishLanguage'))
+  const EnglishIndex = lazy(() => import('@/2_main/english/_indexes/EnglishIndex'));
+  const English = lazy(() => import('@/2_main/english/English'));
+  const TheEnglishLanguage = lazy(() => import('@/2_main/english/TheEnglishLanguage'));
 
   // 2. Grammar
-  const GrammarIndex = lazy(() => import('./2_main/english/_indexes/GrammarIndex'))
-  const Adverb = lazy(() => import('./2_main/english/grammar/Adverb'))
-  const Verb = lazy(() => import('./2_main/english/grammar/Verb'))
-  const Adjective = lazy(() => import('./2_main/english/grammar/Adjective'))
-  const Noun = lazy(() => import('./2_main/english/grammar/Noun'))
+  const GrammarIndex = lazy(() => import('@/2_main/english/_indexes/GrammarIndex'));
+  const Adverb = lazy(() => import('@/2_main/english/grammar/Adverb'));
+  const Verb = lazy(() => import('@/2_main/english/grammar/Verb'));
+  const Adjective = lazy(() => import('@/2_main/english/grammar/Adjective'));
+  const Noun = lazy(() => import('@/2_main/english/grammar/Noun'));
   
   // 3. Pronunciation
-  const PronunciationIndex = lazy(() => import('./2_main/english/_indexes/PronunciationIndex'))
-  const Pronunciation = lazy(() => import('./2_main/english/pronunciation/Pronunciation'))
-  const IPA = lazy(() => import('./2_main/english/pronunciation/IPA'))
+  const PronunciationIndex = lazy(() => import('@/2_main/english/_indexes/PronunciationIndex'));
+  const Pronunciation = lazy(() => import('@/2_main/english/pronunciation/Pronunciation'));
+  const IPA = lazy(() => import('@/2_main/english/pronunciation/IPA'));
 
   // 4. Tiếng Anh
-  const TiengAnhIndex = lazy(() => import('./2_main/english/_indexes/TiengAnhIndex'))
-  const BayDauPhuPhienAmTiengAnh1 = lazy(() => import('./2_main/english/tienganh/BayDauPhuPhienAmTiengAnh1'))
-  const BayDauPhuPhienAmTiengAnh2 = lazy(() => import('./2_main/english/tienganh/BayDauPhuPhienAmTiengAnh2'))
-  const NguyenAmDon = lazy(() => import('./2_main/english/tienganh/NguyenAmDon'))
-  const NguyenAmDoi = lazy(() => import('./2_main/english/tienganh/NguyenAmDoi'))
-  const DichCumDanhTuTiengAnh = lazy(() => import('./2_main/english/tienganh/DichCumDanhTuTiengAnh'))
-  const DichCumDongTuTiengAnh = lazy(() => import('./2_main/english/tienganh/DichCumDongTuTiengAnh'))
-  const DichCauHoiTiengAnh = lazy(() => import('./2_main/english/tienganh/DichCauHoiTiengAnh'))
+  const TiengAnhIndex = lazy(() => import('@/2_main/english/_indexes/TiengAnhIndex'));
+  const BayDauPhuPhienAmTiengAnh1 = lazy(() => import('@/2_main/english/tienganh/BayDauPhuPhienAmTiengAnh1'));
+  const BayDauPhuPhienAmTiengAnh2 = lazy(() => import('@/2_main/english/tienganh/BayDauPhuPhienAmTiengAnh2'));
+  const NguyenAmDon = lazy(() => import('@/2_main/english/tienganh/NguyenAmDon'));
+  const NguyenAmDoi = lazy(() => import('@/2_main/english/tienganh/NguyenAmDoi'));
+  const DichCumDanhTuTiengAnh = lazy(() => import('@/2_main/english/tienganh/DichCumDanhTuTiengAnh'));
+  const DichCumDongTuTiengAnh = lazy(() => import('@/2_main/english/tienganh/DichCumDongTuTiengAnh'));
+  const DichCauHoiTiengAnh = lazy(() => import('@/2_main/english/tienganh/DichCauHoiTiengAnh'));
   
   // 5. Vocabulary - Tính chính xác
-  const VocabularyIndex = lazy(() => import('./2_main/english/_indexes/VocabularyIndex'))
-  const Accurately1 = lazy(() => import('./2_main/english/vocabulary/Accurately1'))
-  const Accurately2 = lazy(() => import('./2_main/english/vocabulary/Accurately2'))
-  const Precisely1 = lazy(() => import('./2_main/english/vocabulary/Precisely1'))
-  const Precisely2 = lazy(() => import('./2_main/english/vocabulary/Precisely2'))
-  const Correctly1 = lazy(() => import('./2_main/english/vocabulary/Correctly1'))
-  const Correctly2 = lazy(() => import('./2_main/english/vocabulary/Correctly2'))
-  const Exactly1 = lazy(() => import('./2_main/english/vocabulary/Exactly1'))
-  const Exactly2 = lazy(() => import('./2_main/english/vocabulary/Exactly2'))
+  const VocabularyIndex = lazy(() => import('@/2_main/english/_indexes/VocabularyIndex'));
+  const Accurately1 = lazy(() => import('@/2_main/english/vocabulary/Accurately1'));
+  const Accurately2 = lazy(() => import('@/2_main/english/vocabulary/Accurately2'));
+  const Precisely1 = lazy(() => import('@/2_main/english/vocabulary/Precisely1'));
+  const Precisely2 = lazy(() => import('@/2_main/english/vocabulary/Precisely2'));
+  const Correctly1 = lazy(() => import('@/2_main/english/vocabulary/Correctly1'));
+  const Correctly2 = lazy(() => import('@/2_main/english/vocabulary/Correctly2'));
+  const Exactly1 = lazy(() => import('@/2_main/english/vocabulary/Exactly1'));
+  const Exactly2 = lazy(() => import('@/2_main/english/vocabulary/Exactly2'));
 
   // 6. Vocabulary - Tính tỉ mỉ
-  const Painstakingly1 = lazy(() => import('./2_main/english/vocabulary/Painstakingly1'))
-  const Painstakingly2 = lazy(() => import('./2_main/english/vocabulary/Painstakingly2'))
-  const Thoroughly1 = lazy(() => import('./2_main/english/vocabulary/Thoroughly1'))
-  const Thoroughly2 = lazy(() => import('./2_main/english/vocabulary/Thoroughly2'))
-  const Diligently1 = lazy(() => import('./2_main/english/vocabulary/Diligently1'))
-  const Diligently2 = lazy(() => import('./2_main/english/vocabulary/Diligently2'))
-  const Scrupulously1 = lazy(() => import('./2_main/english/vocabulary/Scrupulously1'))
-  const Scrupulously2 = lazy(() => import('./2_main/english/vocabulary/Scrupulously2'))
+  const Painstakingly1 = lazy(() => import('@/2_main/english/vocabulary/Painstakingly1'));
+  const Painstakingly2 = lazy(() => import('@/2_main/english/vocabulary/Painstakingly2'));
+  const Thoroughly1 = lazy(() => import('@/2_main/english/vocabulary/Thoroughly1'));
+  const Thoroughly2 = lazy(() => import('@/2_main/english/vocabulary/Thoroughly2'));
+  const Diligently1 = lazy(() => import('@/2_main/english/vocabulary/Diligently1'));
+  const Diligently2 = lazy(() => import('@/2_main/english/vocabulary/Diligently2'));
+  const Scrupulously1 = lazy(() => import('@/2_main/english/vocabulary/Scrupulously1'));
+  const Scrupulously2 = lazy(() => import('@/2_main/english/vocabulary/Scrupulously2'));
 
 // 3. Test Tailwind
-	const TestTailwind = lazy(() => import('./components/TestTailwind'))
+	const TestTailwind = lazy(() => import('@/components/TestTailwind'));
 
 
 // Xuất khẩu chức năng
-export default function AppRoutes() {
+export default function AppRoutes(): React.JSX.Element {
   return (
 
   <Suspense fallback={<></>}>
